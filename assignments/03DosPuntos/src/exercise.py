9ff22c940fd466ac8e77f31984a30e5df0d4e323
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-17 13:37:45 -0700

    Update exercise.py

--- assignments/03DosPuntos/src/exercise.py
+++ assignments/03DosPuntos/src/exercise.py
@@ -1,8 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    ma=float(input("Calificación de la materia: "))
+    mb=float(input("Calificación de la materia: "))
+    mc=float(input("Calificación de la materia: "))
+    md=float(input("Calificación de la materia: "))
+    mp=(ma+mb+mc+md)/4
+    print("El promedio es:",mp)
 
 if __name__ == '__main__':
     main()
