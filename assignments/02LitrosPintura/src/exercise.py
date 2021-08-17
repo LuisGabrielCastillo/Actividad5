29d8b579b905badafb935cac07a6089a75f80e7b
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-17 13:36:12 -0700

    Update exercise.py

--- assignments/02LitrosPintura/src/exercise.py
+++ assignments/02LitrosPintura/src/exercise.py
@@ -1,9 +1,11 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    asp=float(input(""))
+    mpl=float(input(""))
+    from math import ceil
+    clp=ceil(asp/mpl)
+    print(clp)
+    
 if __name__ == '__main__':
     main()
