8294dc7d544684f2ec6320fad6be54a09f38a81f
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-17 11:18:56 -0700

    Update exercise.py

--- assignments/01DiagonalRect/src/exercise.py
+++ assignments/01DiagonalRect/src/exercise.py
@@ -1,10 +1,7 @@
 import math
 def main():
     #escribe tu código abajo de esta línea   
-    a=float(input())
-    b=float(input())
-    d=math.sqrt(math.pow(a,2)+math.pow(b,2))
-    print(d)
+    pass
 
 
 
