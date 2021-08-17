ced3781afb056fca07e5f330ebed0accb3774d85
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-17 16:25:35 -0700

    Update exercise.py

--- assignments/05Poblacion/src/exercise.py
+++ assignments/05Poblacion/src/exercise.py
@@ -1,10 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    pobi=int(input(""))
+    tiem=int(input(""))
+    tasa=float(input(""))
+    import math
+    pobf=math.floor(pobi*math.e**(tiem-tasa))
+    print(pobf)
 
 if __name__ == '__main__':
     main()
