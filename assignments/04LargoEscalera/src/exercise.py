d4ccdf379e106483314ba7c6c05fc505c051f9cc
LuisGabrielCastillo <89027056+LuisGabrielCastillo@users.noreply.github.com>
2021-08-17 14:59:36 -0700

    Update exercise.py

--- assignments/04LargoEscalera/src/exercise.py
+++ assignments/04LargoEscalera/src/exercise.py
@@ -1,9 +1,11 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    altc=float(input(""))
+    angl=float(input(""))
+    from math import sin, ceil
+    lare=ceil((altc/(sin(angl))))
+    print(lare)
 
 if __name__ == '__main__':
     main()
