de578befc60b5b75bce084555b052d533759194c
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-17 11:22:08 -0700

    Update exercise.py

--- assignments/04LargoEscalera/src/exercise.py
+++ assignments/04LargoEscalera/src/exercise.py
@@ -1,10 +1,7 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    alto=int(input())
-    ang=int(input())
-    largo= alto/math.sin(ang)
-    print(math.ceil(largo))
+    pass
 
 
 
