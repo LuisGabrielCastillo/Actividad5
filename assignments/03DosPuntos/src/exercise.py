e8dd42291c2c94fdb9f10e4429292fb8d3fd36d8
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-17 11:21:23 -0700

    Update exercise.py

--- assignments/03DosPuntos/src/exercise.py
+++ assignments/03DosPuntos/src/exercise.py
@@ -1,12 +1,7 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    x1=float(input())
-    y1=float(input())
-    x2=float(input())
-    y2=float(input())
-    d=math.sqrt(math.pow(x2-x1,2)+math.pow(y2-y1,2))
-    print(d)
+    pass
 
 
 if __name__ == '__main__':
